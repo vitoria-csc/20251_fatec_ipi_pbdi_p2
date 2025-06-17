57d3becfce15dae4c86d7da25b80b5dd9eed4197
Rodrigo Bossini <professorbossini@gmail.com>
2025-06-17 00:21:00 -0300

    first commit

---
+++ script.py
@@ -0,0 +1,46 @@
+#faça os imports que julgar necessários
+
+
+#essa função deve devolver a base de dados
+def ler_base():
+  pass
+
+#essa função recebe a base lida anteriormente
+#ela deve devolver uma tupla contendo as features e a classe
+def dividir_em_features_e_classe(base):
+  pass
+
+#essa função recebe as features
+#ela deve devolver as features da seguinte forma
+#Valores faltantes da coluna "Gastos com pesquisa e desenvolvimento": substituir pela média
+#Valores faltantes da coluna "Gastos com administracao": substituir pela mediana
+#Valores faltantes da coluna "Gastos com marketing": Substituir por zero
+#Valores faltantes da coluna "Estado": Substituir pela moda
+def lidar_com_valores_faltantes(features):
+  pass
+
+#essa função recebe as features
+#ela deve devolver as features da seguinte forma
+#Variável "Estado": Codificar com OneHotEncoding
+def codificar_categoricas(features):
+  pass
+
+#essa função recebe as features e a classe
+#ela deve devolver uma tupla com 4 itens
+# features de treinamento, features de teste, classe de treinamento, classe de teste
+# a base de treinamento deve ter 75% das instâncias
+def obter_bases_de_treinamento_e_teste(features, classe):
+  pass
+
+#essa função recebe as features de treinamento e de teste
+#ela deve devolver uma tupla com 2 itens, da seguinte forma
+#todas as variáveis normalizadas com o método MinMax
+def normalizar(features_treinamento, features_teste):
+  pass
+
+def vai():
+  #chame as suas funções aqui
+  #exiba as quatro bases aqui
+   pass
+
+vai()
