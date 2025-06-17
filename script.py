8ba28b0b5f5eeda4f0e68886ee3ce070f0f4c158
Rodrigo Bossini <professorbossini@gmail.com>
2025-06-17 09:11:49 -0300

    feat: includes reqs

--- script.py
+++ script.py
@@ -1,15 +1,19 @@
-#faça os imports que julgar necessários
+#REQ 1
+# faça os imports que julgar necessários
 
 
+#REQ 2
 #essa função deve devolver a base de dados
 def ler_base():
   pass
 
+#REQ 3
 #essa função recebe a base lida anteriormente
 #ela deve devolver uma tupla contendo as features e a classe
 def dividir_em_features_e_classe(base):
   pass
 
+#REQ 4
 #essa função recebe as features
 #ela deve devolver as features da seguinte forma
 #Valores faltantes da coluna "Gastos com pesquisa e desenvolvimento": substituir pela média
@@ -19,12 +23,14 @@ def dividir_em_features_e_classe(base):
 def lidar_com_valores_faltantes(features):
   pass
 
+#REQ 5
 #essa função recebe as features
 #ela deve devolver as features da seguinte forma
 #Variável "Estado": Codificar com OneHotEncoding
 def codificar_categoricas(features):
   pass
 
+#REQ 6
 #essa função recebe as features e a classe
 #ela deve devolver uma tupla com 4 itens
 # features de treinamento, features de teste, classe de treinamento, classe de teste
@@ -32,15 +38,17 @@ def codificar_categoricas(features):
 def obter_bases_de_treinamento_e_teste(features, classe):
   pass
 
+#REQ 7
 #essa função recebe as features de treinamento e de teste
 #ela deve devolver uma tupla com 2 itens, da seguinte forma
 #todas as variáveis normalizadas com o método MinMax
 def normalizar(features_treinamento, features_teste):
   pass
 
+#REQ 8
 def vai():
   #chame as suas funções aqui
   #exiba as quatro bases aqui
-   pass
+     pass
 
 vai()
